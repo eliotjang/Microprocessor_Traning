ace7587db0a2ce2077601af99f4fcd7e5132a0d3
eliotjang <eliotjang2@gmail.com>
2019-10-29 20:03:01 +0900

    add project7

--- Project7/uart.c
+++ Project7/uart.c
@@ -6,14 +6,16 @@
 #include <util/delay.h> // for sbi(), _delay_ms()
 #include "uart.h"
 #include "queue.h"
+#define ETX 0x04
 int uart_putchar(char ch, FILE *stream);
 
 FILE Mystdout = FDEV_SETUP_STREAM (uart_putchar, NULL, _FDEV_SETUP_WRITE);
+FILE Mystdin = FDEV_SETUP_STREAM (NULL, uart_getchar, _FDEV_SETUP_READ);
 char volatile uart_busy; // for always access memory
 // uart_busy가 int이던 char이던 상관없음.
 void uart_init()
 {
-	stdout = &Mystdout;
+	stdin = &Mystdin; stdout = &Mystdout;
 	uart_busy = 0;
 	q_init();
 	UBRR0H = 0x00; UBRR0L = 0x07; // 115.2K
@@ -38,6 +40,10 @@ ISR(USART0_RX_vect)
 {
 	char ch;
 	ch = UDR0;
+	if (ch != ETX) {
+		if (ch == '\r')
+			ch = '\n';
+		uart_echo(ch);
 	qi_insert(ch);
 }
 
@@ -60,4 +66,27 @@ int uart_putchar(char ch, FILE *stream)
 	sei();
 	
 	return(1);
+}
+
+int uart_getchar(FILE *stream)
+{
+	char ch;
+	do {
+		cli();
+		ch = qi_delete();
+		sei();
+	} while(ch == 0);
+	
+	if (ch == ETX) return(-1);
+	else return(ch);
+}
+
+void uart_echo(char ch)
+{
+	if (ch == '\n') uart_echo('\r');
+	if (!uart_busy) {
+		UDR0 = ch;
+		uart_busy = 1;
+	}else
+		qo_insert(ch);
 }
--- Project7/uart.h
+++ Project7/uart.h
@@ -1,3 +1,5 @@
 // uart.h
 int uart_putchar(char ch, FILE *stream);
+int uart_getchar(FILE *stream);
 void uart_init(void);
+void uart_echo(char ch);
